aa01b6e49f016991ff9240f1ace51ce41435622f
ship <670185448@qq.com>
2017-04-11 21:40:26 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -43,6 +43,10 @@ end
        redirect_to jobs_path
      end
 
+     def job_params
+         params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+       end
+       
 private
 
 def job_params
