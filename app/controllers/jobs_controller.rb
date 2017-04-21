156ff5dccd04c4396773245501ff0115a11932a0
ship <670185448@qq.com>
2017-04-21 22:11:08 +0800

    管理选项按钮美化，首页板块美化

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,5 @@
 class JobsController < ApplicationController
+before_action :validate_search_key, only: [:search]
 before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def index
     @jobs = case params[:order]
@@ -54,6 +55,26 @@ end
 
        redirect_to jobs_path
      end
+
+     def search
+         if @query_string.present?
+           search_result = Job.ransack(@search_criteria).result(:distinct => true)
+           @jobs = search_result.paginate(:page => params[:page], :per_page => 5 )
+         end
+       end
+
+
+       protected
+
+       def validate_search_key
+         @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+         @search_criteria = search_criteria(@query_string)
+       end
+
+
+       def search_criteria(query_string)
+         { :title_cont => query_string }
+       end
 
 private
 
